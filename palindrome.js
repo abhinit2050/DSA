d883d48cc8cfb846743eed296d943918c2da0adc
Abhinit Rai <abhinit1990@gmail.com>
2025-07-11 11:39:23 +0530

    palindrome method2

--- palindrome.js
+++ palindrome.js
@@ -1,21 +1,42 @@
-let num =4384565489;
+let num =12421;
 let num_copy=num;
 
-let newNum='';
-const numLength = (num.toString().length);
+//Method1 - this method converts number to string
+// let num_copy=num;
+// let newNum='';
+// const numLength = (num.toString().length);
 
-for(let i=0;i<numLength;i++){
+// for(let i=0;i<numLength;i++){
 
-    let lastDigit = num%10;
+//     let lastDigit = num%10;
 
-   newNum = newNum+lastDigit;
-    num = Math.floor(num/10);
+//    newNum = newNum+lastDigit;
+//     num = Math.floor(num/10);
    
+// }
+
+// if(newNum.toString() == num_copy.toString()){
+//     console.log("It is a palindrome!")
+// } else {
+//     console.log("It is not a palindrome!");
+    
+// }
+
+
+//Method2 - this method does not convert number into string
+
+let rev=0;
+
+while(num>0){
+    rev = 10*rev + (num%10);
+    num=Math.floor(num/10);
 }
 
-if(newNum.toString() == num_copy.toString()){
+
+if(num_copy===rev){
     console.log("It is a palindrome!")
-} else {
-    console.log("It is not a palindrome!");
+ } else {
+     console.log("It is not a palindrome!");
     
-}
+ }
+
